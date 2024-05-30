e6660b436c97056c8c129d71d1f802e4e25f63ac
Aleksey Golyudov <alexgolyd@yandex.ru>
2024-05-30 15:35:30 +0300

    develop

--- app/src/main/java/app/hdrezka/youtubesun/MainActivity.kt
+++ app/src/main/java/app/hdrezka/youtubesun/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     YouTubeSunTheme {
-        Greeting("1.0")
+        Greeting("1.0" )
     }
 }
