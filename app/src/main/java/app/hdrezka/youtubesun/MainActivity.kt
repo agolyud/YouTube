b5e543f8ce321881e23dba78e6d72546e5033f40
Aleksey Golyudov <alexgolyd@yandex.ru>
2024-05-30 15:15:58 +0300

    Add ViewMopdel version app

--- app/src/main/java/app/hdrezka/youtubesun/MainActivity.kt
+++ app/src/main/java/app/hdrezka/youtubesun/MainActivity.kt
@@ -4,24 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.hdrezka.youtubesun.ui.theme.YouTubeSunTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val viewModel = MainViewModel()
+        viewModel.setAppVersion(packageManager.getPackageInfo(packageName, 0).versionName)
+
         setContent {
             YouTubeSunTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = viewModel.appVersion.value!!,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +41,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "YouTube version, $name",
         modifier = modifier
     )
 }
@@ -42,6 +50,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     YouTubeSunTheme {
-        Greeting("Android")
+        Greeting("1.0")
     }
 }
