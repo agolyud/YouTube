42fbed4850a906a6a976aa149db189b05f0c689d
Aleksey Golyudov <alexgolyd@yandex.ru>
2024-06-14 19:35:51 +0300

    gradle serialization add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,6 @@ dependencies {
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.kotlinx.serialization.json)
 }
