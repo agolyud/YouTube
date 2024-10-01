5230df0200a3a6397925e8abe3d993980cf0c4e5
Aleksey Golyudov <alexgolyd@yandex.ru>
2024-10-01 17:41:42 +0300

    add mic

--- app/src/main/java/app/youtube/sun/ui/search/SearchScreen.kt
+++ app/src/main/java/app/youtube/sun/ui/search/SearchScreen.kt
@@ -1,5 +1,11 @@
 package app.youtube.sun.ui.search
 
+import android.app.Activity
+import android.content.Intent
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -13,16 +19,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import app.youtube.sun.R
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(navController: NavHostController) {
     var searchText by remember { mutableStateOf(TextFieldValue("")) }
+    val context = LocalContext.current
+
+    val speechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+        onResult = { result ->
+            val spokenText = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            if (result.resultCode == Activity.RESULT_OK && spokenText != null) {
+                searchText = TextFieldValue(spokenText[0] ?: "")
+            } else {
+                Toast.makeText(context, "Voice input failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
 
     Scaffold(
         topBar = {
@@ -58,7 +79,14 @@ fun SearchScreen(navController: NavHostController) {
                             singleLine = true
                         )
 
-                        IconButton(onClick = { /*TODO*/ }) {
+                        IconButton(onClick = {
+                            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                                putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                                putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                                putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now")
+                            }
+                            speechRecognizerLauncher.launch(intent)
+                        }) {
                             Icon(
                                 imageVector = Icons.Default.Mic,
                                 contentDescription = "Mic Search",
