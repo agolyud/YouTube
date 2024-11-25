32d6939d77fabfbe939f389a23da08ce2fae8b06
Aleksey Golyudov <alexgolyd@yandex.ru>
2024-11-25 11:32:14 +0300

    Hilt

---
+++ build-logic/src/main/kotlin/HiltPlugin.kt
@@ -0,0 +1,24 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class HiltPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("dagger.hilt.android.plugin")
+                apply("com.google.devtools.ksp")
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            dependencies {
+                "implementation"(libs.findLibrary("hilt-android").get())
+                "ksp"(libs.findLibrary("hilt-android-compiler").get())
+                "kspAndroidTest"(libs.findLibrary("hilt-android-compiler").get())
+                "androidTestImplementation"(libs.findLibrary("hilt-android-testing").get())
+            }
+        }
+    }
+}
